a52a36b6e0260d3cc918795f270d2674579cab8c
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-04-04 17:01:10 -0500

    Update

--- p24.py
+++ p24.py
@@ -10,13 +10,15 @@ cardinal = lambda beta, t, L, U: 2 * (beta - 1) * beta ** (t - 1) * (U - L + 1) 
 x_min = lambda beta, L: beta ** (L - 1)
 x_max = lambda beta, U, t: beta**U * (1 - beta ** (-t))
 
+
 # print(f"El cardinal de F(beta, t, L, U) es {cardinal(beta=beta, t=t, L=L, U=U)}.")
 # print(f"x_min: {x_min(beta=beta, L=L)}")
 # print(f"x_max: {x_max(beta=beta, U=U, t=t)}")
-def base_2_a_10()
-
-
+# def base_2_a_10():
+#     pass
 
 if __name__ == "__main__":
-    for e in range(L, U + 1):
-        print(f"{1*beta**e}")
+    for b in [0b100, 0b101, 0b110, 0b111]:
+        for e in range(L, U + 1):
+            print(f"{b*beta**e}")
+        print()
